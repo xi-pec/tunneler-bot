a18bef2eb8b506d272398af03378f0d31e363ad1
xi_pec <xepicscripter@gmail.com>
2023-07-06 09:57:11 +0800

    removed unnecessary imports in globals.d.ts

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,8 +1,3 @@
-import { Client, Collection } from "discord.js";
-import { Server } from "./modules/Server.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
 declare global {
     var __filename: string;
     var __dirname: string;
